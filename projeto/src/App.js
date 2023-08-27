f1e9af4ddc7fdaf17889f876cc6198c3d568c26b
Brunnosanttos <brunoohsanttos@gmail.com>
2023-08-27 18:54:16 -0300

    projeto

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -7,6 +7,10 @@ function App() {
     { id: 1, name: 'Pedalada', local: 'Abreu e Lima', data: "15/06/1999", imagem: "imagem1"},
     { id: 2, name: 'Socializar', local: "Ibura, Recife", data: "07/09/2001", imagem: "imagem2"},
     { id: 3, name: 'Caçar tatu na praia', local: "Itamaracá", data: "04/07/2025", imagem: "imagem3"},
+    { id: 4, name: 'Caçar tatu na praia', local: "Itamaracá", data: "04/07/2025", imagem: "imagem3"},
+    { id: 5, name: 'Caçar tatu na praia', local: "Itamaracá", data: "04/07/2025", imagem: "imagem3"},
+    { id: 6, name: 'Caçar tatu na praia', local: "Itamaracá", data: "04/07/2025", imagem: "imagem3"},
+    { id: 7, name: 'Caçar tatu na praia', local: "Itamaracá", data: "04/07/2025", imagem: "imagem3"},
   ]);
 
   return (
